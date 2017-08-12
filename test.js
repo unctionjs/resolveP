18270104df46dd87d6c6603d6df7bf2aabc8eb78
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:06:18 -0700

    Don't bother using the extension, eslint complains

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import resolveP from "./source.js"
+import replaceWhen from "./source"
 
 test(({equal}) => {
   return resolveP("a")
